c49d47c34b32b47c2c68c7b4ae363d6f5f7a5730
CarinaLR <carinalr84@gmail.com>
2020-02-27 16:32:00 -0500

    debugging

--- app/components/Home.js
+++ app/components/Home.js
@@ -8,13 +8,13 @@ class Home extends Component {
       <div className="home">
         <h1>Juzmooth</h1>
         <p>Habitos sanos para todos los dias.</p>
-        <img
+        {/* <img
           id="imgHome"
           src={
             'https://images.unsplash.com/photo-1496318447583-f524534e9ce1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1405&q=80'
           }
-        />
-        {/* <img id="imgHome" source={logo} /> */}
+        /> */}
+        <img id="imgHome" src={require('../assets/jLogo.png')} />
       </div>
     );
   }
