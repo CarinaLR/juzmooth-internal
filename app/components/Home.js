a74fcb3568a56f7d676e6ec630922d5b012c6871
CarinaLR <carinalr84@gmail.com>
2020-03-02 10:21:30 -0500

    moving assets folder

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
-import logo from '../assets/jLogo.png';
 
 class Home extends Component {
   render() {
     console.log('here');
     return (
       <div className="home">
-        <h1>Juzmooth</h1>
-        <p>Habitos sanos para todos los dias.</p>
-        {/* <img
-          id="imgHome"
+        <p>
+          <h1>Habitos sanos para todos los dias.</h1>
+        </p>
+        <img className="imgHome" src={'./assets/jLogo.png'} />
+        <img
+          className="imgHome"
           src={
             'https://images.unsplash.com/photo-1496318447583-f524534e9ce1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1405&q=80'
           }
-        /> */}
-        <img id="imgHome" src={logo} />
+        />
       </div>
     );
   }
--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
-        use: ['file-loader'],
+        use: [{ loader: 'file-loader', options: {} }],
       },
     ],
   },
