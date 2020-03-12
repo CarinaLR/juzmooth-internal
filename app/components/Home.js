dd58277d46ae1f9c9f91bcda0f762919904ed1a8
CarinaLR <carinalopez@Carinas-MacBook-Pro.local>
2020-03-12 14:23:22 -0400

    changes

--- app/components/Home.js
+++ app/components/Home.js
@@ -9,13 +9,7 @@ class Home extends Component {
           <h1>Habitos sanos para todos los dias.</h1>
         </p>
         <img className="imgHome" src={"./assets/juzmooth.png"} />
-        <img className="imgHome" src={"./assets/jFront"} />
-        {/* <img
-          className="imgHome"
-          src={
-            'https://images.unsplash.com/photo-1496318447583-f524534e9ce1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1405&q=80'
-          }
-        /> */}
+        <img className="imgHome" src={"./assets/jFront.png"} />
       </div>
     );
   }
