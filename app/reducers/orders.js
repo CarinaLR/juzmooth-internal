3cccb8beef4547190108919350013feb31fa7f0d
CarinaLR <carinalopez@Carinas-MacBook-Pro.local>
2020-03-19 14:50:18 -0400

    adding actions creators

--- app/reducers/orders.js
+++ app/reducers/orders.js
@@ -4,13 +4,42 @@ import axios from "axios";
 const ADD_TO_ORDER = "ADD_TO_ORDER";
 const CREATE_NEW_ORDER = "CREATE_NEW_ORDER";
 const CHECKOUT = "CHECKOUT";
-const GET_CUSTOMER = "GET_CUSTOMER";
-const REMOVE_CUSTOMER = "REMOVE_CUSTOMER";
 const REMOVE_PRODUCT = "REMOVE_PRODUCT";
 const REMOVE_ONE = "REMOVE_ONE";
 const DELETE_ALL = "DELETE_ALL";
 
+//INITIAL STATE
+
+const initialState = [];
+
 //ACTION CREATORS
+export const createNewOrder = () => ({
+  type: CREATE_NEW_ORDER
+});
+
+export const addToOrder = product => ({
+  type: ADD_TO_ORDER,
+  product
+});
+
+export const removeOne = product => ({
+  type: REMOVE_ONE,
+  product
+});
+
+export const checkout = () => ({
+  type: CHECKOUT
+});
+
+export const removeProduct = productId => ({
+  type: REMOVE_PRODUCT,
+  productId
+});
+
+export const deleteAllFromOrder = product => ({
+  type: DELETE_ALL,
+  product
+});
 
 //MIDDLEWARE AND REDUX-THUNK
 
